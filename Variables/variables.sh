4af80882aab0118ae260732a29131433af5b5a92
techsavvyabhi <104838635+Abhi050200@users.noreply.github.com>
2025-03-08 08:44:05 +0000

    c

--- Variables/variables.sh
+++
@@ -1,9 +0,0 @@
-#Variables store data (strings, numbers, etc) and can be used throughout the script
-
-#!/bin/bash                         # Shebang: Specifies the script should be run using Bash
-
-a=1                                # Assigning value 1 to variable 'a'
-b=2                                # Assigning value 2 to variable 'b'
-
-echo "a is $a and b is $b"         # Printing the values of variables
-
